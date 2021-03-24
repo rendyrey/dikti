59fd5232bdbf34501886e2ef95eb7fc47cd301de
rendyrey <rendyreynaldy@gmail.com>
2021-03-24 17:29:39 +0700

    Update Berita.php

--- application/controllers/Berita.php
+++ application/controllers/Berita.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Berita extends CI_Controller {
 
-  public function __construct()
+  public function __construct() 
   {
     parent::__construct();
     $this->load->model('M_berita');
