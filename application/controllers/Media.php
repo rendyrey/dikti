1b4076b7df1f6be607f933842cb11d5b692cbd3f
Rendy Reynaldy <rendy@dropsuite.com>
2021-03-24 18:57:20 +0700

    hapus 1 line

--- application/controllers/Media.php
+++ application/controllers/Media.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Media extends CI_Controller {
 
-
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('M_berita');
