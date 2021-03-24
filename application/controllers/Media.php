aacf1eaae521dad0631c468d0581d2e604ab4fd0
Rendy Reynaldy <rendy@dropsuite.com>
2021-03-24 18:04:29 +0700

    tambah function test math

--- application/controllers/Media.php
+++ application/controllers/Media.php
@@ -26,6 +26,10 @@ class Media extends CI_Controller {
 		}
 	}
 
+	public function testMath(){
+		
+	}
+
 	public function index(){
 		$this->check_login();
 		$query = $this->M_media->get_media_group();
