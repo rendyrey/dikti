b9c1437b42c315f59a165d5f1c0e3938c7adb858
Rendy Reynaldy <rendy@dropsuite.com>
2021-03-24 18:10:26 +0700

    dashboardmaster

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
+	public function DashBoardMaster(){
+		
+	}
 	public function __construct()
 	{
 		parent::__construct();
