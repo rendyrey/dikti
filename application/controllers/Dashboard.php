e023b8e2c262b43236ea4c71d07d53dd35fea4d4
Rendy Reynaldy <rendy@dropsuite.com>
2021-03-24 17:27:17 +0700

    hilangin

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,8 +6,8 @@ class Dashboard extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('M_berita'); 
-		$this->load->model('M_narasumber'); 
+		$this->load->model('M_berita');
+		$this->load->model('M_narasumber');
 		$this->load->model('M_dashboard');
 	}
 	/**
